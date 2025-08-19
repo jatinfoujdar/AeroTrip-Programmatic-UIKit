163bd7e6a716eeef31ca105c85085ab722cb85f3
Jatin Foujdar <jatin321001@gmail.com>
2025-08-19 13:49:39 +0530

    first commit

--- AeroTrip-Programmatic-UIKit/ViewController.swift
+++ AeroTrip-Programmatic-UIKit/ViewController.swift
@@ -4,16 +4,27 @@
 //
 //  Created by jatin foujdar on 19/08/25.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        
+        let label = UILabel()
+        label.text = "Welcome to AeroTrip!"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+
+        // Center the label
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
 
